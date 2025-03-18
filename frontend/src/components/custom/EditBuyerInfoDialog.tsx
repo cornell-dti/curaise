e48b31595757752d73648eef6c61d4984bf33f31
Steven Yu <stevenyuser@gmail.com>
2025-03-17 20:40:46 -0400

    added edit account info and form

---
+++ frontend/src/components/custom/EditBuyerInfoDialog.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { UserSchema, UpdateUserBody } from "common";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export function EditBuyerInfoDialog({
+  user,
+  token,
+}: {
+  user: z.infer<typeof UserSchema>;
+  token: string;
+}) {
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof UpdateUserBody>>({
+    resolver: zodResolver(UpdateUserBody),
+    defaultValues: {
+      name: user.name,
+      venmoUsername: user.venmoUsername ?? undefined,
+    },
+  });
+
+  async function updateUser(data: z.infer<typeof UpdateUserBody>) {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_API_URL + "/user/" + user.id,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    const result = await response.json();
+    if (!response.ok) {
+      toast.error(result.message);
+      return;
+    } else {
+      router.refresh();
+      toast.success(result.message);
+      setOpen(false);
+    }
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Info</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit((data) => updateUser(data))}>
+            <DialogHeader>
+              <DialogTitle>Edit account info</DialogTitle>
+              <DialogDescription>
+                Make changes to your account here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="venmoUsername"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Venmo Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="No Username" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
