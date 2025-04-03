02b4390eab2503e32e74e9f0430f8707f124658f
Eric Weng <ew522@cornell.edu>
2025-04-02 21:21:03 -0400

    Implemented getFundraiser

--- frontend/src/app/buyer/fundraiser/[id]/page.tsx
+++ frontend/src/app/buyer/fundraiser/[id]/page.tsx
@@ -1,8 +1,60 @@
+import { connection } from "next/server";
+import { CompleteFundraiserSchema } from "common";
+
+const getFundraiser = async (id: string) => {
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/fundraiser/${id}`,
+	);
+	const result = await response.json();
+	if (!response.ok) {
+		throw new Error(result.message);
+	}
+	const data = CompleteFundraiserSchema.safeParse(result.data);
+	if (!data.success) {
+		throw new Error("Could not parse fundraiser data");
+	}
+	return data.data;
+};
+
+const getFundraiserItems = async (id: string) => { 
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/fundraiser/${id}/items`,
+  );
+  const result = await response.json();
+  if (!response.ok) {
+    throw new Error(result.message);
+  }
+  const data = CompleteFundraiserSchema.array().safeParse(result.data);
+  if (!data.success) {
+    throw new Error("Could not parse fundraiser items data");
+  }
+  return data.data;
+}
+
 export default async function FundraiserPage({
-  params,
+	params,
 }: {
-  params: Promise<{ id: string }>;
+	params: Promise<{ id: string }>;
 }) {
+  await connection();
+  
   const id = (await params).id;
-  return <div>My Fundraiser: {id}</div>;
+  const fundraiser = await getFundraiser(id);
+  // const fundraiserItems = await getFundraiserItems(id);
+
+  return (
+		<div>
+			<h1>{fundraiser.name}</h1>
+			<p>{fundraiser.description}</p>
+			<h2>Items</h2>
+			{/* <ul>
+				{fundraiserItems.map((item) => (
+					<li key={item.id}>
+						<h3>{item.name}</h3>
+						<p>{item.description}</p>
+					</li>
+				))}
+			</ul> */}
+		</div>
+	);
 }
