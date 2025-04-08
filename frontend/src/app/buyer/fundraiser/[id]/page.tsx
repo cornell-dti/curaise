8a88cda40ab48d6d7c7728d9ecbf33a522c7d94c
Steven Yu <stevenyuser@gmail.com>
2025-04-08 16:43:36 -0400

    fix fundraisergalleryslider

--- frontend/src/app/buyer/fundraiser/[id]/page.tsx
+++ frontend/src/app/buyer/fundraiser/[id]/page.tsx
@@ -59,7 +59,7 @@ export default async function FundraiserPage({
           <FundraiserGallerySlider images={fundraiser.imageUrls} />
         )}
 
-        <h1 className="text-3xl font-bold mb-2">{fundraiser.name}</h1>
+        <h1 className="text-3xl font-bold my-2">{fundraiser.name}</h1>
         <p className="text-gray-600 mb-4">{fundraiser.description}</p>
 
         <div className="flex flex-col items-start w-full space-y-2">
