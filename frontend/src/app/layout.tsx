8fa0dda39b8121a292c1116063e3d21d98e61923
Arsh-S <write2arshsingh@gmail.com>
2025-04-18 15:53:32 -0400

    added fonts to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,29 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Manrope, Nunito, Outfit, Work_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const work_sans = Work_Sans({
   subsets: ["latin"],
   variable: "--font-inter",
 });
