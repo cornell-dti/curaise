bae1532c573ae2f4205d1b664bdb94ee9710b580
Steven Yu <stevenyuser@gmail.com>
2025-04-25 15:49:02 -0400

    fix next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		domains: ["otkgrzwqkcbykhldiksv.supabase.co"],
-	},
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "otkgrzwqkcbykhldiksv.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
