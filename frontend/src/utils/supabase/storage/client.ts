b72d49a9c4d76cba5f33da76bdfc3941f804d2bc
Steven Yu <stevenyuser@gmail.com>
2025-04-25 15:13:02 -0400

    fix uploadimage

--- frontend/src/utils/supabase/storage/client.ts
+++ frontend/src/utils/supabase/storage/client.ts
@@ -1,34 +1,57 @@
 import imageCompression from "browser-image-compression";
 import { createClient } from "../client";
-interface UploadImageProps {
-	file: File;
-	bucket: string;
-	folder?: string;
-}
 
-export async function UploadImage({ file, bucket, folder }: UploadImageProps) {
-	const fileName = file.name;
-	const path = `${folder ? folder + "/" : ""}${fileName}`;
+export const uploadImage = async ({
+  file,
+  bucket,
+  folder,
+}: {
+  file: File;
+  bucket: string;
+  folder?: string;
+}): Promise<
+  | {
+      imageUrl: string;
+      error: null;
+    }
+  | {
+      imageUrl: null;
+      error: string;
+    }
+> => {
+  const fileName = file.name;
+  const path = `${folder ? folder + "/" : ""}${fileName}`;
 
-	try {
-		file = await imageCompression(file, {
-			maxSizeMB: 1,
-		});
-	} catch (error) {
-		console.error(error);
-		return { imageUrl: "", error: "Error compressing image" };
-	}
+  try {
+    file = await imageCompression(file, {
+      maxSizeMB: 1,
+    });
+  } catch (error) {
+    console.error(error);
+    return {
+      imageUrl: null,
+      error: "Error compressing image",
+    };
+  }
 
-	const supabase = createClient();
-	const storage = supabase.storage;
+  const supabase = createClient();
 
-	const { data, error } = await storage.from(bucket).upload(path, file);
+  const { data, error } = await supabase.storage
+    .from(bucket)
+    .upload(path, file);
 
-	if (error) {
-		console.error(error);
-		return { imageUrl: "", error: "Error uploading image" };
-	}
-	const imageUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/object/public/${bucket}/${data?.path}`;
+  if (error) {
+    console.error(error.message);
 
-	return { imageUrl, error: "" };
-}
+    return {
+      imageUrl: null,
+      error: `Error uploading image: ${error.message}`,
+    };
+  }
+  const imageUrl = `${process.env
+    .NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/object/public/${bucket}/${
+    data.path
+  }`;
+
+  return { imageUrl, error: null };
+};
