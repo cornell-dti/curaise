9afba950dae59c521eb7f415a1bef606c2546015
Arsh-S <write2arshsingh@gmail.com>
2025-09-19 17:35:01 -0400

    setup cheerio

--- backend/src/api/email/email.handlers.ts
+++ backend/src/api/email/email.handlers.ts
@@ -1,19 +1,32 @@
 import { Request, Response } from "express-serve-static-core";
+import * as cheerio from "cheerio";
 import { PostmarkInboundEmailBody } from "./email.types";
 
-// TODO
-export const parseEmailHandler = async (
+
+// // TODO
+// export const parseEmailHandler = async (
+//   req: Request<{}, any, PostmarkInboundEmailBody, {}>,
+//   res: Response
+// ) => {
+//   // https://postmarkapp.com/developer/user-guide/inbound/parse-an-email
+
+//   // parse email HtmlBody
+//   req.body.HtmlBody;
+
+//   // verify email
+
+//   // save email transaction to database
+
+//   res.status(200).json({ message: "Email parsed" });
+// };
+
+export const parseVerifiedVenmoAccountEmailHandler = async (
   req: Request<{}, any, PostmarkInboundEmailBody, {}>,
   res: Response
 ) => {
-  // https://postmarkapp.com/developer/user-guide/inbound/parse-an-email
-
   // parse email HtmlBody
-  req.body.HtmlBody;
-
-  // verify email
-
-  // save email transaction to database
-
-  res.status(200).json({ message: "Email parsed" });
+  const $ = cheerio.load(req.body.HtmlBody);
+  
+  
+  
 };
