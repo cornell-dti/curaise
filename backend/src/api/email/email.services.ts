e53e3de254678ff34e6603ae1320e1aba25f205b
chelsea-she <11chelseashe@gmail.com>
2025-09-28 17:41:52 -0400

    got rid of qp parsing

--- backend/src/api/email/email.services.ts
+++ backend/src/api/email/email.services.ts
@@ -1,16 +1,10 @@
 import { load } from "cheerio";
-import qp from "quoted-printable";
 
 export const parseUnverifiedVenmoEmail = (raw: string) => {
-  // decode quoted-printable if present
-  const htmlString = /=\r?\n|=3D|=20|=[A-F0-9]{2}/i.test(raw)
-    ? qp.decode(raw)
-    : raw;
-
   let parsedAmount: number | null = null;
   let orderId: string | null = null;
 
-  const $ = load(htmlString);
+  const $ = load(raw);
   const amount = $('span[style="color:#148572;float:right;"]').text().trim();
   parsedAmount = parseFloat(amount.replace("$", "").replace("+", ""));
   if (isNaN(parsedAmount)) {
