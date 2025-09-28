d1ec56152a4dee0eb0c2e49c47bfb54eb055f9e0
Arsh-S <write2arshsingh@gmail.com>
2025-09-28 16:01:00 -0400

    Refactor Verified Venmo email parsing to use Decimal.js for accurate amount calculations and remove unecessary logging

--- backend/src/api/email/email.services.ts
+++ backend/src/api/email/email.services.ts
@@ -1,30 +1,26 @@
 import { load } from "cheerio";
+import { Decimal } from "decimal.js";
 
 export const parseVerifiedVenmoEmail = (raw: string) => {
-  // Load HTML into Cheerio
-  const $ = load(raw);
+    // Load HTML into Cheerio
+    const $ = load(raw);
 
-  // Extract data
-  const dollarAmount = $('div.amount-container__amount-text').text().trim() || '0'; // "5"
-  const centAmount = $('div.amount-container__text-high').eq(1).text().trim() || '00'; // "01"
-  const transactionNote = $('p.transaction-note').text().trim() || 'NO NOTE'; // "4a1s"
+    // Extract data
+    const dollarAmount = $("div.amount-container__amount-text").text().trim() || "0"; // "5"
+    const centAmount = $("div.amount-container__text-high").eq(1).text().trim() || "00"; // "01"
+    const transactionNote = $("p.transaction-note").text().trim() || "NO NOTE"; // "4a1s"
 
-  // Convert to integers
-  const dollarAmountInt = parseInt(dollarAmount, 10);
-  const centAmountInt = parseInt(centAmount, 10);
+    // Convert to integers
+    const dollarAmountInt = parseInt(dollarAmount, 10);
+    const centAmountInt = parseInt(centAmount, 10);
 
-  // Convert to float for total amount
-  const floatAmount = dollarAmountInt + centAmountInt / 100;
+    // Convert to Decimal.js value
+    const parsedAmount = new Decimal(dollarAmountInt).plus(new Decimal(centAmountInt).dividedBy(100));
 
-  console.log('Dollar Amount:', `"${dollarAmountInt}"`);
-  console.log('Cent Amount:', `"${centAmountInt}"`);
-  console.log('Total Amount:', `$${floatAmount.toFixed(2)}`);
-  console.log('Transaction Note:', `"${transactionNote}"`);
+    // Validate parsed amount
+    if (isNaN(parsedAmount.toNumber())) {
+        throw new Error("Failed to parse payment amount");
+    }
 
-  // Validate parsed amount
-  if (isNaN(floatAmount)) {
-    throw new Error("Failed to parse payment amount");
-  }
-
-  return { parsedAmount: floatAmount, orderId: transactionNote };
+    return {Decimal: parsedAmount, orderId: transactionNote };
 };
