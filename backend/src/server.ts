07214255ab7a2d977f1c9e1dde7a97a83c3a5112
Frank <sd924@cornell.edu>
2025-10-24 16:34:47 -0400

    Move email related middleware under email route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,20 +6,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: "5mb" })); // Mailgun form posts
-app.use((req, res, next) => {
-  if (req.originalUrl === "/api/email/parse") {
-    console.log("=== Incoming Mailgun POST ===");
-    console.log("Content-Type:", req.headers["content-type"]);
-    console.log("Keys:", Object.keys(req.body));
-    console.log(
-      "Body Preview:",
-      JSON.stringify(req.body, null, 2).slice(0, 1000)
-    );
-    console.log("=============================");
-  }
-  next();
-});
 app.use(router);
 
 app.listen(process.env.PORT, () => {
