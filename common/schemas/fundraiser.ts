df76c88a6757623fbe55bf3f3cd858ec5d4ab8fe
Steven Yu <stevenyuser@gmail.com>
2025-11-07 14:13:02 -0500

    added fundraiser pickup event zod

--- common/schemas/fundraiser.ts
+++ common/schemas/fundraiser.ts
@@ -8,6 +8,13 @@ export const AnnouncementSchema = z.object({
   createdAt: z.coerce.date(),
 });
 
+export const PickupEventSchema = z.object({
+  id: z.string().uuid(),
+  startsAt: z.coerce.date(),
+  endsAt: z.coerce.date(),
+  location: z.string(),
+});
+
 export const BasicFundraiserSchema = z.object({
   id: z.string().uuid(),
   name: z.string().min(1).max(255),
@@ -16,13 +23,11 @@ export const BasicFundraiserSchema = z.object({
   venmoUsername: z.string().min(1).max(255).nullish(),
   venmoEmail: z.string().min(1).max(255).nullish(),
   goalAmount: MoneySchema.nullish(),
-  pickupLocation: z.string(),
   buyingStartsAt: z.coerce.date(),
   buyingEndsAt: z.coerce.date(),
-  pickupStartsAt: z.coerce.date(),
-  pickupEndsAt: z.coerce.date(),
   organization: BasicOrganizationSchema,
   imageUrls: z.array(z.string().url()),
+  pickupEvents: z.array(PickupEventSchema),
 });
 
 export const CompleteFundraiserSchema = BasicFundraiserSchema.extend({
@@ -31,6 +36,19 @@ export const CompleteFundraiserSchema = BasicFundraiserSchema.extend({
 
 // CRUD BODIES:
 
+export const CreatePickupEventBody = z.object({
+  startsAt: z.coerce.date(),
+  endsAt: z.coerce.date(),
+  location: z.string(),
+});
+
+export const UpdatePickupEventBody = z.object({
+  startsAt: z.coerce.date(),
+  endsAt: z.coerce.date(),
+  location: z.string(),
+});
+
+// fundraisers must be created with at least one pickup event
 export const CreateFundraiserBody = z.object({
   name: z.string().min(1).max(255),
   description: z.string(),
@@ -50,14 +68,13 @@ export const CreateFundraiserBody = z.object({
     .pipe(z.string().email().optional()),
   imageUrls: z.array(z.string().url()),
   goalAmount: MoneySchema.optional(),
-  pickupLocation: z.string(),
   buyingStartsAt: z.coerce.date(),
   buyingEndsAt: z.coerce.date(),
-  pickupStartsAt: z.coerce.date(),
-  pickupEndsAt: z.coerce.date(),
   organizationId: z.string().uuid(),
+  pickupEvents: z.array(CreatePickupEventBody).min(1),
 });
 
+// pickup events are updated/deleted separately
 export const UpdateFundraiserBody = z.object({
   name: z.string().min(1).max(255),
   description: z.string(),
@@ -76,12 +93,9 @@ export const UpdateFundraiserBody = z.object({
     )
     .pipe(z.string().email().optional()),
   goalAmount: MoneySchema.optional(),
-  pickupLocation: z.string(),
   imageUrls: z.array(z.string().url()),
   buyingStartsAt: z.coerce.date(),
   buyingEndsAt: z.coerce.date(),
-  pickupStartsAt: z.coerce.date(),
-  pickupEndsAt: z.coerce.date(),
 });
 
 export const CreateFundraiserItemBody = z.object({
