5ad3e3b181d5eb5c9b5d4b26961916483b334e47
Chelsea She <cms556@cornell.edu>
2025-11-16 16:07:26 -0500

    updated frontend visualizing pickup events

--- frontend/src/app/seller/fundraiser/[id]/components/FundraiserTop.tsx
+++ frontend/src/app/seller/fundraiser/[id]/components/FundraiserTop.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { CompleteFundraiserSchema, CompleteItemSchema } from "common";
+import {
+  CompleteFundraiserSchema,
+  CompleteItemSchema,
+  PickupEventSchema,
+} from "common";
 import { Calendar, MapPin } from "lucide-react";
 import { format } from "date-fns";
 import Link from "next/link";
@@ -26,6 +30,23 @@ export function FundraiserTop({
     setStep(step);
     setOpenEdit(true);
   };
+  const grouped_days = fundraiser.pickupEvents
+    .slice()
+    .sort(
+      (a, b) => new Date(a.startsAt).getTime() - new Date(b.startsAt).getTime()
+    )
+    .reduce(
+      (
+        acc: Record<string, z.infer<typeof PickupEventSchema>[]>,
+        event: z.infer<typeof PickupEventSchema>[][number]
+      ) => {
+        const dayKey = format(event.startsAt, "EEEE, M/d/yyyy");
+        acc[dayKey] = acc[dayKey] || [];
+        acc[dayKey].push(event);
+        return acc;
+      },
+      {}
+    );
   const published = (publish: boolean) => {
     setPublish(publish);
   };
@@ -90,15 +111,26 @@ export function FundraiserTop({
             </Button>
           </div>
         </div>
-        <div className="flex gap-4 min-w-[80vw]">
-          <span className="flex gap-2 items-center">
-            <Calendar className="h-5" />{" "}
-            {format(fundraiser.buyingStartsAt, "EEEE, M/d/yyyy")} -{" "}
-            {format(fundraiser.buyingEndsAt, "EEEE, M/d/yyyy")}
-          </span>
-          <span className="flex gap-2">
-            <MapPin className="h-5" /> {fundraiser.pickupLocation}
-          </span>
+        <div className="flex flex-col gap-4 min-w-[80vw]">
+          {Object.entries(grouped_days).map(([day, events]) => (
+            <div
+              key={day}
+              className="text-[18px] flex items-center gap-12 min-w-[80vw]"
+            >
+              <span className="flex gap-2 items-center">
+                <Calendar className="h-5" /> {format(day, "EEEE, M/d/yyyy")}
+              </span>
+              <div>
+                {events.map((e) => (
+                  <div key={e.id} className="flex items-center gap-2">
+                    <MapPin className="h-5" />
+                    {e.location}, {format(e.startsAt, "h:mm aa")} to{" "}
+                    {format(e.endsAt, "h:mm aa")}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
         {!publish && (
           <div className="mt-6 flex flex-col gap-3">
